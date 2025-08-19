9718e7a7c74006c19198b5c900c098d502f6f60c
Nikos Charalabopoulos <n.h.charalabopoulos@gmail.com>
2025-08-19 13:51:33 +0300

    I made the createPet to add pets and linked it in the router.post

--- backend/routes/petRoutes.js
+++ backend/routes/petRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express.Router()
-const {getAllPets, getPetById} = require('../controllers/petController')
+const {getAllPets, getPetById, createPet} = require('../controllers/petController')
 
 router.get('/',getAllPets)
 router.get('/:id',getPetById)
+router.post('/', createPet)
 
 
 module.exports = router
