3929b8acc069bcab56b0be7eebf2ca0af93eb39e
Anita Angelidou <anitawebdesigns@gmail.com>
2025-08-22 22:34:02 +0300

    completed user routes

--- backend/routes/usersRoutes.js
+++ backend/routes/usersRoutes.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const { getUsers, getUser } = require("../controllers/userController");
+const { getUsers, getUser, updateUser, deleteUser } = require("../controllers/userController");
 const authenticateToken = require("../middleware/authToken");
+const validateUser = require("../validations/userValidations");
 
 router.get("/", authenticateToken, getUsers);
 router.get("/:id", authenticateToken, getUser);
+router.put("/:id", authenticateToken, validateUser, updateUser);
+router.delete("/:id", authenticateToken, deleteUser);
 
 module.exports = router;
